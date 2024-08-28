35b6887f2cb56009f69f59b02f0cfe62ff116af6
Nouman Khan <khannouman083@gmail.com>
2024-08-28 17:01:31 +0500

    Hello this is my check new commit

--- lib/check.dart
+++ lib/check.dart
@@ -10,6 +10,6 @@ class mynewApp extends StatefulWidget {
 class _mynewAppState extends State<mynewApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
