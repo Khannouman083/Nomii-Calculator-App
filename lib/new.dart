b0c0be08b6dbd91a03520e1226ee3f9efbefb4ae
Nouman Khan <khannouman083@gmail.com>
2024-08-28 17:03:45 +0500

    Hello this commit

---
+++ lib/new.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class mynewerapp extends StatefulWidget {
+  const mynewerapp({super.key});
+
+  @override
+  State<mynewerapp> createState() => _mynewerappState();
+}
+
+class _mynewerappState extends State<mynewerapp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
